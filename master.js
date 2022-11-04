7492c4d11bbf4e0e52a7f27502b66ac8dd5d31ca
Luca Torriani <luca.torriani@codiceplastico.com>
2022-11-04 17:05:28 +0100

    added more tests and logic for the functioning

--- master.js
+++ master.js
@@ -1,8 +1,37 @@
-module.exports = {
-  evaluate: (secret, guess) => {
-    if (guess[0] == secret[0]) {
-      return [1, 0]
-    } 
-    return [0, 0]
+const evaluate = (secret, guess) => {
+  let correctColors = 0,
+      correctPositions = 0 
+
+    correctPositions = findCorrectPosition(secret, guess)
+    correctColors = findCorrectColors(secret, guess)
+
+    return [correctPositions, correctColors]
+}
+
+const findCorrectPosition = (secret, guess) => {
+  let correctPositions = 0
+
+  correctPositions = secret.reduce((p, current, index) => {
+    return p + (current == guess[index])
+  }, 0)
+
+  return correctPositions
+}
+
+const findCorrectColors = (secret, guess) => {
+  let correctColors = 0
+
+  for (let i = 0; i < secret.length; i++) {
+    for (let j = 0; j < secret.length; j++) {
+      if (secret[i] == guess[j] && i != j) {
+        correctColors++
+      }  
+    }
   }
+
+  return correctColors
+}
+
+module.exports = {
+  evaluate
 }
--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 const master = require('./master')
 
+//[red, green, blue, yellow]
+
 test('one wrong color in wrong position', () => {
   const result = master.evaluate(['blue'], ['red'])
   expect(result).toStrictEqual([0, 0])
@@ -8,4 +10,29 @@ test('one wrong color in wrong position', () => {
 test('one correct color in correct position', () => {
   const result = master.evaluate(['blue'], ['blue'])
   expect(result).toStrictEqual([1, 0])
+})
+
+test('Two colors, one correct but in the wrong position', () => {
+  const result = master.evaluate(['red', 'yellow'], ['blue', 'red'])
+  expect(result).toStrictEqual([0, 1])
+})
+
+test('Three colors, one in correct position', () => {
+  const result = master.evaluate(['red', 'yellow', 'blue'], ['green', 'green', 'blue'])
+  expect(result).toStrictEqual([1, 0])
+})
+
+test('Four colors, one in correct position, one correct but in wrong positon', () => {
+  const result = master.evaluate(['red', 'yellow', 'blue', 'green'], ['red', 'blue', 'a', 'a'])
+  expect(result).toStrictEqual([1, 1])
+})
+
+test('Four colors, all wrong colors and positions', () => {
+  const result = master.evaluate(['red', 'yellow', 'blue', 'green'], ['a', 'a', 'a', 'a'])
+  expect(result).toStrictEqual([0, 0])
+})
+
+test('Four colors, all correct positions and color', () => {
+  const result = master.evaluate(['red', 'yellow', 'blue', 'green'], ['red', 'yellow', 'blue', 'green'])
+  expect(result).toStrictEqual([4, 0])
 })
